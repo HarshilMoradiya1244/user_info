01d6da8e1e1b0a33985959cf21ade5b86787487a
vivek <Katariyavivek228@gmail.com>
2023-12-28 16:59:07 +0530

    new

--- lib/utils/json_helper.dart
+++ lib/utils/json_helper.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:user_info/screen/country/model/country_model.dart';
 import 'package:user_info/screen/user/model/user_model.dart';
-
 import '../screen/user_info/model/userinfomodel.dart';
 
 class JsonHelper {
@@ -14,22 +13,19 @@ class JsonHelper {
     return modelList;
   }
 
-
   Future<List<CountryNameModel>> CountryToList() async {
     var jsonString = await rootBundle.loadString('assets/json/country.json');
     List post = jsonDecode(jsonString);
-
     // post.map((e) => print(e['name'])).toList();
-
     List<CountryNameModel> modelList = post.map((e) => CountryNameModel.mapToModel(e['name'])).toList();
     return modelList;
   }
 
   Future<UserInfoModel> UserToModel()
   async {
-    var useresString = await rootBundle.loadString("assets/json/userinfo.json");
-    var useres = jsonDecode(useresString);
-    UserInfoModel useresModel= UserInfoModel.mapToModel(useres);
+    var userString = await rootBundle.loadString("assets/json/userinfo.json");
+    var user= jsonDecode(userString);
+    UserInfoModel userInfoModel = UserInfoModel.mapToModel(user);
     return UserInfoModel();
   }
 }
